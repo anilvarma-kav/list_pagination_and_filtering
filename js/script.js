471097f5b4260c10eba45c2e896e9e43d501ec72
Anil Varma <anilvarma.kav@gmail.com>
2019-08-11 00:20:04 -0600

    basic functionality
    Basic functionality of the application is completed.

--- js/script.js
+++ js/script.js
@@ -2,49 +2,53 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+const studentList = document.getElementsByClassName('student-item cf');
+const maxNumber = 10;
+
+const showPage = (studentList, pageNum) => {
+    const start_index = (pageNum * maxNumber) - maxNumber;
+    const end_index =pageNum * maxNumber;
+    for (let i = 0; i<studentList.length; i++) {
+        let li = studentList[i];
+        if(i >= start_index && i < end_index){
+            li.style.display = '';
+        }
+        else{
+            li.style.display = 'none';
+        }
+    }
+};
+showPage(studentList, 1);
+const appendPageLinks = (studentList) => {
+    const numberOfPages = parseInt(studentList.length/maxNumber)+1;
+    console.log(numberOfPages);
+    const pageDiv = document.querySelector('.page');
+    const div = document.createElement('div');
+    div.className = 'pagination';
+    pageDiv.appendChild(div);
+    const ul = document.createElement('ul');
+    div.appendChild(ul);
+    for (let i = 1; i <= numberOfPages; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = i.toString();
+        ul.appendChild(li);
+        li.appendChild(a);
+        a.addEventListener('click' , (e) => {
+            const link = e.target;
+            const pageNum = link.textContent;
+            showPage(studentList, pageNum);
+            const pageLinks = document.querySelectorAll('.pagination a');
+            for (let j = 0; j < pageLinks.length; j++) {
+                pageLinks[j].classList.remove('active');
+            }
+            link.className = 'active';
+        });
+    }
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
+};
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(studentList);
